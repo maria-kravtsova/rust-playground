7da8d5e800d3f19f701a7a12414c5c7f7a1ffdf8
Maria Kravtsova <kravtsova.mariia@gmail.com>
2020-01-01 16:30:21 -0500

    Learn about rust files and cargo

---
+++ hello_cargo/target/debug/deps/hello_cargo-dda01d1e560fb586.d
@@ -0,0 +1,5 @@
+/Users/mariakravtsova/code/rust-playground/hello_cargo/target/debug/deps/hello_cargo-dda01d1e560fb586: src/main.rs
+
+/Users/mariakravtsova/code/rust-playground/hello_cargo/target/debug/deps/hello_cargo-dda01d1e560fb586.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/mariakravtsova/code/rust-playground/hello_cargo/target/debug/hello_cargo: /Users/mariakravtsova/code/rust-playground/hello_cargo/src/main.rs
